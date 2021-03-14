67f936c596bb34e53af05c2c68b6d2bb85c7080c
sateeshtcs <sateeshkumar.ponnaganti@tcs.com>
2021-03-14 13:32:42 +0530

    changes committed to master branch

--- index2.js
+++ index2.js
@@ -3,3 +3,4 @@ console.log('Weclome to Index2');
 console.log('Weclome to Index2');
 console.log('Weclome to Index24 master');
 console.log('Weclome to Index24 dev');
+console.log('Weclome to Index24 masterBranch');
